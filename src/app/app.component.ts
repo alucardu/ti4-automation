2ca90392f05afa36b4a5d2c30e1355791e92963d
Peter Boomsma <peter@cleanpixel.nl>
2023-02-12 23:25:41 +0100

    Initalized Apollo, GraphQL and Prisma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const GET_HELLO = gql`
+  query ExampleQuery {
+    hello
+  }
+`
+
+type Hello = {
+  hello: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ti4-automation';
+
+export class AppComponent implements OnInit {
+  public title = 'ti4-automation';
+  public loading = false;
+  public something: string = "Old world..."
+
+  constructor(private apollo: Apollo) {}
+
+  public ngOnInit(): void {
+    this.apollo.query<Hello>({
+      query: GET_HELLO
+    }).subscribe((
+      { data, loading}) => {
+        this.loading = true;
+        setTimeout(() => {
+          this.something = data.hello;
+          this.loading = loading;
+        }, 2500)
+      })
+  }
 }
