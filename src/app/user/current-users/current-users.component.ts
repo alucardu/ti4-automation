d69a60c8c6ec37565b3dd13975f56d8e0c8b16c2
Peter Boomsma <peter@cleanpixel.nl>
2023-03-07 16:41:40 +0100

    Now showing players on mobile

--- src/app/user/current-users/current-users.component.ts
+++ src/app/user/current-users/current-users.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Session } from 'src/types/sessionTypes';
+import { BreakpointObserver,  BreakpointState, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-current-users',
   templateUrl: './current-users.component.html',
   styleUrls: ['./current-users.component.scss'],
 })
-export class CurrentusersComponent {
+export class CurrentusersComponent implements OnInit {
   @Input() session!: Session;
+  public showPlayers = true;
+  public showPlayerToggleBtn = false;
+
+  readonly breakpoint$ = this.breakpointObserver;
+  currentBreakpoint = '';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.Small])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.showPlayers = false;
+          this.showPlayerToggleBtn = true;
+        }
+      });
+  }
+
+  public togglePlayers(): void {
+    this.showPlayers = !this.showPlayers;
+  }
 }
