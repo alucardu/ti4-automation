4468ef8bf7298fd2b65eca87d1804e51e500e478
Peter Boomsma <peter@cleanpixel.nl>
2023-03-08 10:23:14 +0100

    fixed scrolling issue in chatbox

--- src/app/messages/display-messages/display-messages.component.ts
+++ src/app/messages/display-messages/display-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { GET_MESSAGES } from 'src/operations/messageOperations/queries';
 import { Session } from 'src/types/sessionTypes';
@@ -10,15 +10,26 @@ import { GetMessages } from 'src/types/messageTypes';
   templateUrl: './display-messages.component.html',
   styleUrls: ['./display-messages.component.scss'],
 })
-export class DisplayMessagesComponent implements OnInit {
+export class DisplayMessagesComponent implements OnInit, AfterViewInit {
   @Input() session!: Session;
 
   protected messages$ = this.messageService.messages$;
 
-  constructor(private apollo: Apollo, private messageService: MessageService) {}
+  constructor(
+    private apollo: Apollo,
+    private messageService: MessageService,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   public ngOnInit(): void {
     this.getMessages(this.session);
+  }
+
+  public ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.renderer.setStyle(this.el.nativeElement, "max-height", `${this.el.nativeElement.offsetHeight}px`);
+    });
   }
 
   public getMessages(session: Session): void {
